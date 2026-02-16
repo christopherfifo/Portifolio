0cb55a3c58f8b9f1cfb333925369caa12c195ca6
Christopher Willians <163358050+christopherfifo@users.noreply.github.com>
2026-02-16 20:02:35 -0300

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Navbar from './components/navbar'
+
+function App() {
+  return (
+    <>
+      <section className=''>
+        <Navbar />
+      </section>
+    </>
+  )
+}
+
+export default App
